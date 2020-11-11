3d11e4657efb27f7321b39e3a75436e01f507e66
Tomas <tmerendo@everis.com>
2020-11-11 18:19:27 +0100

    first commit

---
+++ project/Dependecies.scala
@@ -0,0 +1,25 @@
+import sbt._
+
+object Dependecies {
+  object version {
+    val akka = "2.5.9"
+    val akkaHttp = "10.0.11"
+  }
+
+  /** ********* PROD DEPENDENCIES **************** */
+  val produccion = Seq(
+    "com.github.nscala-time" %% "nscala-time" % "2.22.0",
+    "com.lihaoyi" %% "pprint" % "0.5.6",
+    "com.typesafe.akka" %% "akka-actor" % version.akka,
+    "com.typesafe.akka" %% "akka-stream" % version.akka,
+    "com.typesafe.akka" %% "akka-http" % version.akkaHttp
+  )
+
+  /** ********* TEST DEPENDENCIES **************** */
+  val test = Seq(
+    "org.scalatest" %% "scalatest" % "3.1.1" % Test,
+    "org.scalamock" %% "scalamock" % "4.4.0" % Test,
+    "com.typesafe.akka" %% "akka-testkit" % version.akka % Test,
+    "com.typesafe.akka" %% "akka-http-testkit" % version.akkaHttp % Test
+  )
+}
